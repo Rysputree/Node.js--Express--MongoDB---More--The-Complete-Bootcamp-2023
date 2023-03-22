ae52e3d03fd8d791a2ec1653ce13a97ff8a792b7
YUIMORII <yuimoriistudio@gmail.com>
2023-03-22 01:31:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,15 @@ app.all('*', (req, res, next) => {
   });
 });
 
-app.use((err, req, res, next) => {});
+app.use((err, req, res, next) => {
+  //internal server error 500
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 //app.get('/api/v1/tours', getAllTours);
 //app.post('/api/v1/tours', createTour);
